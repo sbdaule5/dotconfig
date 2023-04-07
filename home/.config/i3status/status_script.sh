c07f9df6883c1bc5a86a832e91d9cbaebf854dae
Shubham <sbdaule5@gmail.com>
2023-04-07 23:21:39 +0530

    Set up music
    Setup mpd, add keybindings for controlling music(alpha numeric keys).
    Create dmenu script using ytfzf that will search youtube for video add
    it to mpd playlist and display notification with thumbnail. i3status
    will show music info when music is being played. Changed qutebrowser to
    play music using mpd.

---
+++ home/.config/i3status/status_script.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# This i3status wrapper allows to add custom information in any position of the statusline
+# It was developed for i3bar (JSON format)
+
+# The idea is to define "holder" modules in i3status config and then replace them
+
+# In order to make this example work you need to add
+# order += "tztime holder__hey_man"
+# and 
+# tztime holder__hey_man {
+#        format = "holder__hey_man"
+# }
+# in i3staus config 
+
+# Don't forget that i3status config should contain:
+# general {
+#   output_format = i3bar
+# }
+#
+# and i3 config should contain:
+# bar {
+#   status_command exec /path/to/this/script.sh
+# }
+
+# Make sure jq is installed
+# That's it
+
+# You can easily add multiple custom modules using additional "holders"
+
+function update_holder {
+
+  local instance="$1"
+  local replacement="$2"
+  echo "$json_array" | jq --argjson arg_j "$replacement" "(.[] | (select(.instance==\"$instance\"))) |= \$arg_j" 
+}
+
+function remove_holder {
+
+  local instance="$1"
+  echo "$json_array" | jq "del(.[] | (select(.instance==\"$instance\")))"
+}
+
+function music {
+
+
+  text=$(mpc status -f "[[%artist%] - [%title%]]"| awk '{if (NR==1) head=$0; if(NR==2){if($1 == "[playing]"){print head " " $3}}}' | sed -e 's/\"//g')
+  local json='{ "full_text": "'$text'", "color": "#2787e7" }'
+  json_array=$(update_holder holder__music "$json")
+}
+
+i3status | (read line; echo "$line"; read line ; echo "$line" ; read line ; echo "$line" ; while true
+do
+  read line
+  json_array="$(echo $line | sed -e 's/^,//')"
+  music
+  echo ",$json_array" 
+done)
