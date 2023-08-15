971402b10c867fda8b99c1d9e488adc269727cf3
Shubham <sbdaule5@gmail.com>
2023-08-16 01:36:48 +0530

    Fix formatting in nvim-tree.lua

--- home/.config/nvim/after/plugin/nvim-tree.lua
+++ home/.config/nvim/after/plugin/nvim-tree.lua
@@ -3,27 +3,26 @@ local function nvim_tree_attach(bufnr)
 
 	local function opts(desc)
       return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-    end
+  end
 
-    -- default mappings
-    api.config.mappings.default_on_attach(bufnr)
+  -- default mappings
+  api.config.mappings.default_on_attach(bufnr)
 
-    -- custom mappings
-    vim.keymap.set('n', '<C-t>',          api.tree.change_root_to_parent,  opts('Up'))
-    vim.keymap.set('n', '<C-H>',          api.tree.change_root_to_parent,  opts('Up'))
-    vim.keymap.set('n', '<Space><Space>', api.tree.change_root_to_node,    opts('Cd'))
-    vim.keymap.set('n', '?',              api.tree.toggle_help,            opts('Help'))
-    vim.keymap.set('n', 'zh',             api.tree.toggle_hidden_filter,   opts('Toggle Hidden'))
+  -- custom mappings
+  vim.keymap.set('n', '<C-t>',          api.tree.change_root_to_parent,  opts('Up'))
+  vim.keymap.set('n', '<C-H>',          api.tree.change_root_to_parent,  opts('Up'))
+  vim.keymap.set('n', '<Space><Space>', api.tree.change_root_to_node,    opts('Cd'))
+  vim.keymap.set('n', '?',              api.tree.toggle_help,            opts('Help'))
+  vim.keymap.set('n', 'zh',             api.tree.toggle_hidden_filter,   opts('Toggle Hidden'))
 end
 
-require("nvim-tree").setup({
-	view = {
-		width = 30,
-	},
+require("nvim-tree").setup( {
+    view = {
+        width = 30,
+    },
     filters = {
- 		dotfiles = true,
- 	},
-	on_attach = nvim_tree_attach,
-	hijack_unnamed_buffer_when_opening = true,
+        dotfiles = true,
+    },
+    on_attach = nvim_tree_attach,
+    hijack_unnamed_buffer_when_opening = true,
 });
-
