980f504f5aebfbdcc57a8c5d61b408e04cea213e
Shubham <sbdaule5@gmail.com>
2023-08-21 18:20:18 +0530

    In dwm move startup applications to different file
    Hopefully it helps with the startup time.

---
+++ home/.local/bin/startup.sh
@@ -0,0 +1,11 @@
+setsid -f xss-lock --transfer-sleep-lock -- i3lock --nofork -e -p default -i ~/.screen-saver.png
+setsid -f nm-applet
+setsid -f /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+setsid -f systemd-inhibit --what=handle-power-key --who="dwm-dmenushutdown" --why="Shutdown will be handled by dwm" sleep infinity
+setsid -f conky -c .config/conky/conky1.conf -d
+setsid -f picom --config ~/.config/picom/picom.config -b --experimental-backend
+setsid -f kdeconnect-indicator
+setsid -f playerctld
+emacs --daemon
+# clipmenud
+# setsid -f mpd-mpris
