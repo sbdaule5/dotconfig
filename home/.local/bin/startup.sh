e9f2573cb78f08e04016065ab73ffd46d07b4f30
Shubham <sbdaule5@gmail.com>
2023-10-30 20:04:16 +0530

    dwm: fix startup apps

--- home/.local/bin/startup.sh
+++ home/.local/bin/startup.sh
@@ -1,12 +1,13 @@
-setsid -f xss-lock --transfer-sleep-lock -- i3lock --nofork -e -p default -i ~/.screen-saver.png
+setsid -f xss-lock --transfer-sleep-lock -- i3lock --nofork -e -p default -i ~/.local/share/screen-saver.png
 setsid -f nm-applet
 setsid -f /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
 setsid -f systemd-inhibit --what=handle-power-key --who="dwm-dmenushutdown" --why="Shutdown will be handled by dwm" sleep infinity
 setsid -f conky -c .config/conky/conky1.conf -d
 setsid -f picom --config ~/.config/picom/picom.config -b
-setsid -f kdeconnect-indicator
+# setsid -f kdeconnect-indicator
 setsid -f playerctld
 setsid -f dunst
 emacs --daemon
+$HOME/.fehbg
 # clipmenud
 # setsid -f mpd-mpris
