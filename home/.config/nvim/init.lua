4c396cfddb35c4ec09d918a2a9d28d4725d007b3
Shubham Daule <sbdaule5@gmail.com>
2024-02-02 20:48:56 +0530

    nvim: Source all .vim files in config directory

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -9,9 +9,16 @@ xdg_config_home = os.getenv("XDG_CONFIG_HOME")
 if xdg_config_home ~= nil then
   xdg_config_home = os.getenv("HOME").."/.config"
 end
-file = io.open(xdg_config_home.."/nvim/vimscript.vim")
-if file ~= nil then
-  vim.cmd(file:read("*a"))
-else
-  vim.api.nvim_err_writeln("File vimscript.vim does not exist")
+
+local vimFiles = {"vimscript.vim", "notes.vim"}
+
+for _,vimFile in ipairs(vimFiles) do
+  local fullFilePath = xdg_config_home..'/nvim/'..vimFile
+  local file = io.open(fullFilePath)
+  if file ~= nil then
+    vim.cmd(file:read("*a"))
+    io.close(file)
+  else
+    vim.api.nvim_err_writeln("File "..fullFilePath.." does not exist")
+  end
 end
