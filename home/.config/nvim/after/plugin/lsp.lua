3ab6ca6e72b7adeb9a1a6d1f6e7b5fd52005d220
Shubham Daule <sbdaule5@gmail.com>
2024-02-02 20:26:22 +0530

    nvim: lsp: Add configuration options for pyton, rust and lua lsp
    - Add vim and bash lsp

--- home/.config/nvim/after/plugin/lsp.lua
+++ home/.config/nvim/after/plugin/lsp.lua
@@ -36,19 +36,91 @@ vim.api.nvim_create_autocmd('LspAttach', {
   end
 })
 
-local lsp_capabilities = require('cmp_nvim_lsp').default_capabilities()
--- refer for the server names in lspconfig
--- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+local cmp_nvim_lsp = require('cmp_nvim_lsp')
+local lsp_capabilities = cmp_nvim_lsp.default_capabilities()
 local nvim_lsp = require('lspconfig')
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 nvim_lsp.clangd.setup{ capabilities = lsp_capabilities, handlers = handlers, cmd = {"/usr/bin/clangd", "--background-index", "--header-insertion=never"}}
 nvim_lsp.tsserver.setup{ capabilities = lsp_capabilities, handlers = handlers}
 nvim_lsp.cssls.setup{ capabilities = lsp_capabilities, handlers = handlers}
 nvim_lsp.emmet_ls.setup{ capabilities = lsp_capabilities, handlers = handlers}
-nvim_lsp.luau_lsp.setup{ capabilities = lsp_capabilities, handlers = handlers}
-nvim_lsp.rust_analyzer.setup{ capabilities = lsp_capabilities, handlers = handlers}
+
+nvim_lsp.lua_ls.setup{
+  capabilities = lsp_capabilities,
+  handlers = handlers,
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+          Lua = {
+            runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
+              }
+              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+              -- library = vim.api.nvim_get_runtime_file("", true)
+            }
+          }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end
+}
+
+nvim_lsp.rust_analyzer.setup{
+  capabilities = lsp_capabilities,
+  handlers = handlers,
+  cmd = {"/usr/bin/rust-analyzer"},
+  settings = {
+    ['rust-analyzer'] = {
+      imports = {
+        granularity = {
+          group = "module",
+        },
+        prefix = "self",
+      },
+      cargo = {
+        buildScripts = {
+          enable = true,
+        },
+      },
+      procMacro = {
+        enable = true
+      },
+      diagnostic = {
+        enable = true
+      }
+    }
+  }
+}
+
 nvim_lsp.tsserver.setup{ capabilities = lsp_capabilities, handlers = handlers}
-nvim_lsp.jedi_language_server.setup{ capabilities = lsp_capabilities, handlers = handlers}
+nvim_lsp.pylsp.setup{ capabilities = lsp_capabilities, handlers = handlers,
+  settings = {
+    pylsp = {
+      plugins = {
+        pycodestyle = {
+          ignore = {'W504', 'W501'},
+          maxLineLength = 100
+        }
+      }
+    }
+  }
+}
 nvim_lsp.svelte.setup{ capabilities = lsp_capabilities, handlers = handlers}
 nvim_lsp.texlab.setup{ capabilities = lsp_capabilities, handlers = handlers}
 nvim_lsp.dotls.setup{ capabilities = lsp_capabilities, handlers = handlers}
 nvim_lsp.cmake.setup{ capabilities = lsp_capabilities, handlers = handlers}
+nvim_lsp.vimls.setup{ capabilities = lsp_capabilities, handlers = handlers}
+nvim_lsp.bashls.setup{ capabilities = lsp_capabilities, handlers = handlers}
