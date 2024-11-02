e4a15908ac977bb291b65f663ca15a443dbf0ea4
Shubham Daule <sbdaule5@gmail.com>
2024-11-02 11:45:34 +0530

    update ptpython config

--- home/.config/ptpython/config.py
+++ home/.config/ptpython/config.py
@@ -22,17 +22,9 @@ def configure(repl):
 
     :param repl: `PythonRepl` instance.
     """
-    # Show function signature (bool).
     repl.show_signature = True
-
-    # Show docstring (bool).
     repl.show_docstring = False
-
-    # Show the "[Meta+Enter] Execute" message when pressing [Enter] only
-    # inserts a newline instead of executing the code.
-    repl.show_meta_enter_message = True
-
-    # Show completions. (NONE, POP_UP, MULTI_COLUMN or TOOLBAR)
+    repl.show_meta_enter_message = False
     repl.completion_visualisation = CompletionVisualisation.POP_UP
 
     # When CompletionVisualisation.POP_UP has been chosen, use this
@@ -40,48 +32,21 @@ def configure(repl):
     repl.completion_menu_scroll_offset = 0
 
     # Show line numbers (when the input contains multiple lines.)
-    repl.show_line_numbers = False
-
-    # Show status bar.
+    repl.show_line_numbers = True
     repl.show_status_bar = True
-
-    # When the sidebar is visible, also show the help text.
-    repl.show_sidebar_help = True
-
-    # Swap light/dark colors on or off
-    repl.swap_light_and_dark = False
-
-    # Highlight matching parentheses.
+    repl.show_sidebar_help = False
+    repl.swap_light_and_dark = True
     repl.highlight_matching_parenthesis = True
-
-    # Line wrapping. (Instead of horizontal scrolling.)
     repl.wrap_lines = True
-
-    # Mouse support.
     repl.enable_mouse_support = True
-
-    # Complete while typing. (Don't require tab before the
-    # completion menu is shown.)
-    repl.complete_while_typing = True
-
-    # Fuzzy and dictionary completion.
-    repl.enable_fuzzy_completion = False
+    repl.complete_while_typing = False
+    repl.enable_fuzzy_completion = True
     repl.enable_dictionary_completion = False
-
-    # Vi mode.
-    repl.vi_mode = False
-
-    # Enable the modal cursor (when using Vi mode). Other options are 'Block', 'Underline',  'Beam',  'Blink under', 'Blink block', and 'Blink beam'
+    repl.vi_mode = True
     repl.cursor_shape_config = "Modal (vi)"
-
-    # Paste mode. (When True, don't insert whitespace after new line.)
     repl.paste_mode = False
-
-    # Use the classic prompt. (Display '>>>' instead of 'In [1]'.)
-    repl.prompt_style = "classic"  # 'classic' or 'ipython'
-
-    # Don't insert a blank line after the output.
-    repl.insert_blank_line_after_output = False
+    repl.prompt_style = "ipython"  # 'classic' or 'ipython'
+    repl.insert_blank_line_after_output = True
 
     # History Search.
     # When True, going back in history will filter the history on the records
@@ -89,25 +54,14 @@ def configure(repl):
     # Note: When enable, please disable the `complete_while_typing` option.
     #       otherwise, when there is a completion available, the arrows will
     #       browse through the available completions instead of the history.
-    repl.enable_history_search = False
-
-    # Enable auto suggestions. (Pressing right arrow will complete the input,
-    # based on the history.)
-    repl.enable_auto_suggest = False
-
-    # Enable open-in-editor. Pressing C-x C-e in emacs mode or 'v' in
-    # Vi navigation mode will open the input in the current editor.
-    repl.enable_open_in_editor = True
+    repl.enable_history_search = True
+    repl.enable_auto_suggest = True
+    repl.enable_open_in_editor = False
 
     # Enable system prompt. Pressing meta-! will display the system prompt.
     # Also enables Control-Z suspend.
     repl.enable_system_bindings = True
-
-    # Ask for confirmation on exit.
     repl.confirm_exit = True
-
-    # Enable input validation. (Don't try to execute when the input contains
-    # syntax errors.)
     repl.enable_input_validation = True
 
     # Use this colorscheme for the code.
@@ -115,7 +69,7 @@ def configure(repl):
     # color schemes. See:
     # https://pygments.org/docs/styles/
     # https://pygments.org/demo/
-    repl.use_code_colorscheme("default")
+    repl.use_code_colorscheme("stata-light")
     # A colorscheme that looks good on dark backgrounds is 'native':
     # repl.use_code_colorscheme("native")
 
@@ -123,11 +77,11 @@ def configure(repl):
 
     # repl.color_depth = "DEPTH_1_BIT"  # Monochrome.
     # repl.color_depth = "DEPTH_4_BIT"  # ANSI colors only.
-    repl.color_depth = "DEPTH_8_BIT"  # The default, 256 colors.
-    # repl.color_depth = "DEPTH_24_BIT"  # True color.
+    # repl.color_depth = "DEPTH_8_BIT"  # The default, 256 colors.
+    repl.color_depth = "DEPTH_24_BIT"  # True color.
 
     # Min/max brightness
-    repl.min_brightness = 0.0  # Increase for dark terminal backgrounds.
+    repl.min_brightness = 0.4  # Increase for dark terminal backgrounds.
     repl.max_brightness = 1.0  # Decrease for light terminal backgrounds.
 
     # Syntax.
@@ -202,7 +156,7 @@ def configure(repl):
 # `ptpython/style.py` for all possible tokens.
 _custom_ui_colorscheme = {
     # Blue prompt.
-    "prompt": "bg:#eeeeff #000000 bold",
+    "prompt": "bg:#0997b3 #282c34 bold",
     # Make the status toolbar red.
-    "status-toolbar": "bg:#ff0000 #000000",
+    "status-toolbar": "bg:#e06c75 #282c34",
 }
